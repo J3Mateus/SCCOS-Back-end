63e69fd6a9281c145b69438b4b8d42bd5650c13e
José Mateus <88603343+J3Mateus@users.noreply.github.com>
2022-09-01 10:33:05 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,10 @@ use Illuminate\Support\Facades\Route;
 Route::group(
     ['middleware'=>['auth:sanctum']],function(){
  
+
+    }
+);
+   
 Route::get('/buscar/formulario/servico',[FormularioController::class,'formForServico']);
 Route::get('/buscar/formulario/servico/{id}',[FormularioController::class,'formForServicoOfId']);
 
@@ -61,9 +65,7 @@ Route::put('/atualizar/os/{id}',[OrdemDeServicoController::class,'update']);
 Route::delete('/deletar/os/{id}',[OrdemDeServicoController::class,'destroy']);
 
 Route::post('/logout',[AuthController::class,'logout']);
-    }
-);
-   
+
 Route::post('/registro', [AuthController::class, 'register']);
 Route::post('/login',[AuthController::class,'login']);
 
